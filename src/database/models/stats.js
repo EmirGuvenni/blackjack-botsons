22e2fbc2d5d78403092cc023ea9481053633bbc8
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-15 04:57:10 +0300

    Updated stats model

--- src/database/models/stats.js
+++ src/database/models/stats.js
@@ -3,6 +3,13 @@ const mongoose = require('mongoose');
 const botSchema = mongoose.Schema({
     errs: {type: Number, min: 0, default: 0, required: true},
     help: {type: Number, min: 0, default: 0, required: true},
+    deal: {type: Number, min: 0, default: 0, required: true},
+    invite: {type: Number, min: 0, default: 0, required: true},
+    hit: {type: Number, min: 0, default: 0, required: true},
+    stay: {type: Number, min: 0, default: 0, required: true},
+    double: {type: Number, min: 0, default: 0, required: true},
+    split: {type: Number, min: 0, default: 0, required: true},
+    join: {type: Number, min: 0, default: 0, required: true}
 });
 
 module.exports = mongoose.model("Botstat", botSchema);
