974efe41e9b3ac03d177d5a70b470a27bb6315a4
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-16 19:58:44 +0300

    Fixed an issue where the bot couldn't get the owner

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -2,7 +2,7 @@ const embed = require('discord.js').MessageEmbed;
 
 module.exports = async(client, guild) => {
     // Get the guild owner
-    let owner = await client.users.cache.get(guild.ownerID);
+    let owner = await guild.members.fetch(guild.ownerID);
 
     // Create the developer embed
     let joinEmbed = new embed()
@@ -10,18 +10,18 @@ module.exports = async(client, guild) => {
         .setTitle(guild.name)
         .setDescription([
             `**Guild ID:** ${guild.id}`,
-            `**Owner ID:** ${owner.id}`,
-            `**Owner tag:** ${owner.username}#${owner.discriminator}`
-        ])
+            `**Owner ID:** ${owner.user.id}`,
+            `**Owner tag:** ${owner.user.username}#${owner.user.discriminator}`
+        ]);
     // Send the embed to the developer server
     client.channels.cache.get(process.env.JOIN_CHANNEL).send(joinEmbed);
 
     // Create the client embed
     let newGuildEmbed = new embed()
         .setColor(0xFCFCFC)
-        .setTitle(`Hello ${owner.username}!`)
+        .setTitle(`Hello ${owner.user.username}!`)
         .setDescription([
-            'To get started use the "/random help" command or flip a coin with "/random flip"!',
+            'To get started use the "/bj help" command or start a game with "/bj deal"!',
             `For more info, please visit ${process.env.HOMEPAGE}`
         ]);
     // Send the client embed to the guild owner
