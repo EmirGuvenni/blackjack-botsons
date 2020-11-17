95dc3236c127bedc26ae315d970328e628da8895
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-18 00:19:54 +0300

    Added an erro embed and fixed dealing method

--- src/commands/deal.js
+++ src/commands/deal.js
@@ -1,16 +1,23 @@
 const Game = require('../classes/game');
-
+const embed = require('discord.js').MessageEmbed;
 const Stats = require('../database/models/stats');
 
 module.exports = {
     run: async(client, message) => {
         // Check if there's a game on that channel
-        if(client.games.get(message.channel.id))
-            return message.channel.send("There's already a game on this channel.");
+        if(client.games.get(message.channel.id)) {
+            // Create an error embed
+            let errEmbed = new embed()
+                .setColor(0xFF0000)
+                .setTitle("Error")
+                .setDescription("There's already a game on this channel.");
+            // Send the embed
+            return message.channel.send(errEmbed);
+        }
 
         // Register a new game
-        client.games.set(message.channel.id, new Game(message))
-              .then((game) => game.deal());
+        client.games.set(message.channel.id, new Game(message));
+        client.games.get(message.channel.id).deal();
 
         // Save stats
         try {
