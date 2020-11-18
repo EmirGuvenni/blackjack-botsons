11455d4adb08bc9f21e09fae110f2c79fcaf4336
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-18 04:42:00 +0300

    Made the class simpler and got rid of images

--- src/classes/card.js
+++ src/classes/card.js
@@ -3,69 +3,7 @@ module.exports = class Card {
         let type = Math.floor((Math.random() * 4) + 1);
         let value = Math.floor((Math.random() * 13) + 1);
 
-        let getSuit = () => {
-            switch(type) {
-                case 1:
-                    return "Hearts";
-                case 2:
-                    return "Diamonds";
-                case 3:
-                    return "Spades";
-                case 4:
-                    return "Clubs"
-            }
-        }
-        let getRank = () => {
-            switch(value) {
-                case 1:
-                    return "Ace";
-                case 11:
-                    return "Jack";
-                case 12:
-                    return "Queen";
-                case 13:
-                    return "King";
-                default:
-                    return value;
-            }
-        }
-        let getCard = () => {
-            let img = [];
-            switch(value) {
-                case 1:
-                    img.push("A");
-                    break;
-                case 11:
-                    img.push("J");
-                    break;
-                case 12:
-                    img.push("Q");
-                    break;
-                case 13:
-                    img.push("K");
-                    break;
-                default:
-                    img.push(value);
-                    break;
-            }
-            switch(type) {
-                case 1:
-                    img.push("H");
-                    break;
-                case 2:
-                    img.push("D");
-                    break;
-                case 3:
-                    img.push("S");
-                    break;
-                case 4:
-                    img.push("C");
-                    break;
-            }
-            img.push(".png");
-            return img.join("");
-        }
-        let getValue = () => {
+        this.value = () => {
             switch(value) {
                 case 1:
                     return "a";
@@ -79,10 +17,31 @@ module.exports = class Card {
                     return value;
             }
         }
-
-        this.value = getValue();
-        this.suit = getSuit();
-        this.rank = getRank();
-        this.img = getCard();
+        this.suit = () => {
+            switch(type) {
+                case 1:
+                    return "Hearts";
+                case 2:
+                    return "Diamonds";
+                case 3:
+                    return "Spades";
+                case 4:
+                    return "Clubs"
+            }
+        }
+        this.rank = () => {
+            switch(value) {
+                case 1:
+                    return "Ace";
+                case 11:
+                    return "Jack";
+                case 12:
+                    return "Queen";
+                case 13:
+                    return "King";
+                default:
+                    return value;
+            }
+        }
     }
 }
