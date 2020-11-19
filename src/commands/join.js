7c7170993dddd57af75bd83335b8042fe0624c2f
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-19 03:01:38 +0300

    Added join command

---
+++ src/commands/join.js
@@ -0,0 +1,35 @@
+const Embed = require('discord.js').MessageEmbed;
+
+module.exports = {
+    run: async(client, message) => {
+        let game = client.games.get(message.channel.id);
+        // Check if there's a game on that channel
+        if(!game){
+            // Create an error embed
+            let errEmbed = new Embed()
+                .setColor(0xFF0000)
+                .setTitle("Error")
+                .setDescription("There's no ongoing game on this channel. You could start a new one with command '/bj deal'.");
+            // Send the embed
+            return message.channel.send(errEmbed);
+        }
+        // Check if the player is already in the game or not
+        else if(game.players.get(message.author.id)){
+            // Create an error embed
+            let errEmbed = new Embed()
+                .setColor(0xFF0000)
+                .setTitle("Error")
+                .setDescription("You're already in this game.");
+            // Send the embed
+            return message.channel.send(errEmbed);
+        }
+
+        // Add the player
+        game.join(client, message.author);
+
+        // Save stats
+        client.handlers.get("stats")(client, "join");
+    },
+    aliases: ["commands"],
+    description: "returns the help list"
+}
