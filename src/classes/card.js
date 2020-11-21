a47c946898f506036ae555679ab2d2d48203623e
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-21 16:39:06 +0300

    Removed value since it was duplicate code

--- src/classes/card.js
+++ src/classes/card.js
@@ -1,12 +1,10 @@
 /**
- * @property {string | number} value - Power value to calculate the hand value
  * @property {number} suit
  * @property {number} rank
  * @type {Card}
  */
 module.exports = class Card {
     constructor() {
-        this.value = Math.floor((Math.random() * 13) + 1);
         this.suit = Math.floor((Math.random() * 4) + 1);
         this.rank = Math.floor((Math.random() * 13) + 1);
     }
