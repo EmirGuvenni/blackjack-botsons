3bc7d785f924fb511f136f999249f20a1ec1f442
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-21 16:37:33 +0300

    Moved logic to controller.js

--- src/classes/card.js
+++ src/classes/card.js
@@ -1,53 +1,13 @@
 /**
  * @property {string | number} value - Power value to calculate the hand value
- * @property {string} suit
- * @property {string} rank
+ * @property {number} suit
+ * @property {number} rank
  * @type {Card}
  */
 module.exports = class Card {
     constructor() {
-        let type = Math.floor((Math.random() * 4) + 1);
-        let value = Math.floor((Math.random() * 13) + 1);
-
-        this.value = () => {
-            switch(value) {
-                case 1:
-                    return "a";
-                case 11:
-                    return 10;
-                case 12:
-                    return 10;
-                case 13:
-                    return 10;
-                default:
-                    return value;
-            }
-        }
-        this.suit = () => {
-            switch(type) {
-                case 1:
-                    return "Hearts";
-                case 2:
-                    return "Diamonds";
-                case 3:
-                    return "Spades";
-                case 4:
-                    return "Clubs"
-            }
-        }
-        this.rank = () => {
-            switch(value) {
-                case 1:
-                    return "Ace";
-                case 11:
-                    return "Jack";
-                case 12:
-                    return "Queen";
-                case 13:
-                    return "King";
-                default:
-                    return value;
-            }
-        }
+        this.value = Math.floor((Math.random() * 13) + 1);
+        this.suit = Math.floor((Math.random() * 4) + 1);
+        this.rank = Math.floor((Math.random() * 13) + 1);
     }
 }
