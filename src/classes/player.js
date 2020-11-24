aded6ee2fcb98f46a0d08b7ec04e43b92b213d74
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-24 14:33:36 +0300

    Updated Player
    Moved functions, removed power function from class, added

--- src/classes/player.js
+++ src/classes/player.js
@@ -1,35 +1,80 @@
 const Card = require('./card');
-/**
- * Takes user as parameter
- * @property {string} tag
- * @property {Array} hand
- * @property {Array} [splitHand]
- * @property {string} state
- * @property {number} cash
- * @property {number} bet
- * @property {function} power
- * @type {Player}
- */
-module.exports = class Player{
-    constructor(user) {
-        this.tag = user.tag;
-        this.hand = [];
-        this.splitHand = [];
-        this.power = () => {
-            let power = 0;
-            let aces = 0;
-            // Get the power of each card and add them up
-            for(let card of this.hand){
-                if(typeof card.value !== "string")
-                    power += card.value;
-                else
-                    aces++;
-            }
-            // Return the hand power
-            return power;
+const {client} = require('../index');
+const Embed = require('discord.js').MessageEmbed;
+
+module.exports = {
+    /**
+     * Takes user as parameter
+     * @property {string} tag
+     * @property {Array} hand
+     * @property {Array} [splitHand]
+     * @property {string} state
+     * @property {number} cash
+     * @property {number} bet
+     * @type {Player}
+     */
+    Player: class Player {
+        constructor(user) {
+            this.tag = user.tag;
+            this.hand = [];
+            this.splitHand = [];
+            this.state = "none";
+            this.cash = 1000;
+            this.bet = 0;
         }
-        this.state = "none";
-        this.cash = 1000;
-        this.bet = 0;
+    },
+    /**
+     * Deals a new card
+     * @param {TextChannel | DMChannel | NewsChannel} game
+     * @param {string} userid
+     */
+    hit: function hit(game, userid) {
+        let player = client.games.get(game.id).players.get(userid);
+        player.hand.push(Card);
+        game.send(`**${player.tag}:** ✅Hit`);
+        client.handlers.get("stats")("hit");
+    },
+    /**
+     * Sets players stay state to true
+     * @param {TextChannel | DMChannel | NewsChannel} game
+     * @param {string} userid
+     */
+    stay: function stay(game, userid) {
+        let player = client.games.get(game.id).players.get(userid);
+        player.isStay = true;
+        game.send(`**${player.tag}:** ❌Stay`);
+        client.handlers.get("stats")("stay");
+    },
+    /**
+     * Splits players hand
+     * @param {TextChannel | DMChannel | NewsChannel} game
+     * @param {string} userid
+     */
+    split: function split(game, userid) {
+        let player = client.games.get(game.id).players.get(userid);
+        player.splitHand.push(player.hand.shift());
+        client.handlers.get("stats")("split");
+    },
+    /**
+     * Doubles players bet
+     * @param {TextChannel | DMChannel | NewsChannel} game
+     * @param {string} userid
+     */
+    double: function double(game, userid) {
+        let player = client.games.get(game.id).players.get(userid);
+
+        // Double the bet of the player
+        if(player.cash >= (player.bet *= 2)) {
+            player.cash -= player.bet;
+            player.bet *= 2;
+            game.send(`**${player.tag}:** 💰Double Down`);
+            client.handlers.get("stats")("double");
+        }
+        else
+            client.users.cache.get(userid).send(new Embed()
+                .setColor(0xFF0000)
+                .setTitle("Error")
+                .setDescription("You can't double down. You don't have enough cash.")
+            );
     }
 }
