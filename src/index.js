855b49c9e7123851ad0da5e7535cd27333f13f8b
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-24 14:28:07 +0300

    Added client export

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ require('dotenv').config({path: __dirname + '/.env'});
 const Discord = require('discord.js');
 const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER']});
 const {registerCommands, registerEvents, registerHandlers} = require('./registry');
-const Stats = require('./database/models/stats');
 console.log("Starting Blackjack Botsons...");
 
 (async() => {
@@ -18,3 +17,5 @@ console.log("Starting Blackjack Botsons...");
     await client.user.setActivity("/bj help");
     console.log("Blackjack Botsons is ready");
 })();
+
+module.exports = {client}
