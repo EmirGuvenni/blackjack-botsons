231c5761f6f2b191cad0a3b324a0db808b92c83f
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-12-04 01:07:33 +0300

    Reformatted the player class

--- src/classes/player.js
+++ src/classes/player.js
@@ -1,22 +1,20 @@
-module.exports = {
-    /**
-     * Takes user as parameter
-     * @property {string} tag
-     * @property {Array} hand
-     * @property {boolean} isStand
-     * @property {number} cash
-     * @property {number} bet
-     * @property {string} stats
-     * @type {Player}
-     */
-    Player: class {
-        constructor(user) {
-            this.tag = user.tag;
-            this.hand = [];
-            this.isStand = false;
-            this.cash = 1000;
-            this.bet = 0;
-            this.stats = "";
-        }
+/**
+ * Takes user as parameter
+ * @property {string} tag
+ * @property {Array} hand
+ * @property {boolean} isStand
+ * @property {number} cash
+ * @property {number} bet
+ * @property {string} stats
+ * @type {Player}
+ */
+module.exports = class Player {
+    constructor(user) {
+        this.tag = user.tag;
+        this.hand = [];
+        this.isStand = false;
+        this.cash = 1000;
+        this.bet = 0;
+        this.stats = "";
     }
 }
