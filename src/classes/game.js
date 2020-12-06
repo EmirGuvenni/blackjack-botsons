9336a4ae4439979632a0d87dec05cd757528edec
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-12-06 04:23:52 +0300

    Added joins property

--- src/classes/game.js
+++ src/classes/game.js
@@ -1,5 +1,6 @@
 /**
  * @property {Map.<Snowflake, Player>} players
+ * @property {number} joins - Number of people waiting to join
  * @property {Array} dealer - dealers hand
  * @property {Array} bets - players that put on a bet
  * @property {Array} done - players that made a move
@@ -10,6 +11,7 @@
 module.exports = class Game {
     constructor() {
         this.players = new Map();
+        this.joins = 0;
         this.dealer = [];
         this.bets = [];
         this.done = [];
